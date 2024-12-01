07b9b33930be3b66de2cba5830fd8bc055c93bf1
admin <admin@gmail.com>
2024-12-01 22:50:51 +0530

    sum program included

---
+++ src/Sample.java
@@ -0,0 +1,13 @@
+public class Sample{
+    public static void main(String[] args) {
+        // Declare and initialize the numbers
+        int a = 5;
+        int b = 10;
+
+        // Calculate the sum
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
