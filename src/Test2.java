a53428d528d8c3b74291c89acebbe081c1b36483
admin <admin@gmail.com>
2024-12-01 23:16:39 +0530

    navin commited

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+public class Test2{
+    public static void main(String[] args) {
+        // Declare and initialize the numbers
+        int a = 5;
+        int b = 10;
+
+        // Calculate the sum
+        int divide = a / b;
+
+        // Print the result
+        System.out.println("The multiply of " + a + " and " + b + " is: " + divide );
+    }
+}
