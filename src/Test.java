fbd5ce9f7eb24a4da05909258ebb68703d7a681a
admin <admin@gmail.com>
2024-12-01 23:00:15 +0530

    sabaree commited

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args) {
+        // Declare and initialize the numbers
+        int a = 5;
+        int b = 10;
+
+        // Calculate the sum
+        int multiply = a * b;
+
+        // Print the result
+        System.out.println("The multiply of " + a + " and " + b + " is: " + multiply );
+    }
+}
